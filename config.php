e6c48454e410354d253cc9ea24f78107aa65de99
sds-cpu <sakshishinde210403@gmail.com>
2024-08-28 21:21:49 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('MAILHOST', "smtp.gmail.com");
-
-define('USERNAME', "sakshishinde210403@gmail.com");
-
-define('PASSWORD', "spribepxtfqigbni");
-
-define('SEND_FROM', "sakshishinde210403@gmail.com");
